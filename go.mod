841d2ca6fe4ef1737d15537b7820c07f60191479
Akash Kumar <akp31378@gmail.com>
2025-01-01 23:33:18 +0530

    golang-restapi with database

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module golang-restapi
 
 go 1.23.4
+
+require github.com/mattn/go-sqlite3 v1.14.24
--- main.go
+++ main.go
@@ -1,26 +1,34 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 // struct of data
 type Item struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
-}
-type SearchItemType struct {
+	ID   int64  `json:"id"`
 	Name string `json:"name"`
 }
 
-// set dummy data
-var items = []Item{
-	{ID: 1, Name: "Pen"},
-	{ID: 2, Name: "Copy"},
-	{ID: 3, Name: "Mobile"},
-	{ID: 4, Name: "Laptop"},
+var db *sql.DB
+var items []Item
+
+func init() {
+	var err error
+	db, err = sql.Open("sqlite3", "./storage.db")
+	if err != nil {
+		log.Fatalf("Error DB setup:: %v", err)
+	}
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY, name TEXT)`)
+	if err != nil {
+		log.Fatalf("Error Creating table:: %v", err)
+	}
 }
 
 // helper function to respondError
@@ -32,9 +40,28 @@ func APIErrorResponse(w http.ResponseWriter, statusCode int, message string) {
 
 // get items handler
 func GetItems(w http.ResponseWriter, r *http.Request) {
-	// set the format content-type for http
+	row, err := db.Query(`SELECT * FROM items`)
+	if err != nil {
+		APIErrorResponse(w, http.StatusBadRequest, fmt.Sprintf("Database Error-getitems:: %v", err.Error()))
+		return
+	}
+	defer row.Close()
+	var response []Item
+	for row.Next() {
+		var item Item
+		err := row.Scan(&item.ID, &item.Name)
+		if err != nil {
+			APIErrorResponse(w, http.StatusInternalServerError, fmt.Sprintf("Database Error-scanitem:: %v", err.Error()))
+			return
+		}
+		response = append(response, item)
+	}
+	if response == nil {
+		APIErrorResponse(w, http.StatusNotFound, "items not found")
+		return
+	}
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(items)
+	json.NewEncoder(w).Encode(response)
 }
 
 // create items handler
@@ -42,85 +69,111 @@ func CreateItem(w http.ResponseWriter, r *http.Request) {
 	var newItem Item
 	err := json.NewDecoder(r.Body).Decode(&newItem)
 	if err != nil {
-		APIErrorResponse(w, http.StatusBadRequest, err.Error())
+		APIErrorResponse(w, http.StatusBadRequest, fmt.Sprintf("JSONDecodeError:: %v", err.Error()))
 		return
 	}
+	stmt, err := db.Prepare(`INSERT INTO items (name) VALUES(?)`)
+	if err != nil {
+		APIErrorResponse(w, http.StatusBadRequest, fmt.Sprintf("DatabaseErrorCreateItems:: %v", err.Error()))
+		return
+	}
+	defer stmt.Close()
+	resp, err := stmt.Exec(newItem.Name)
+	if err != nil {
+		APIErrorResponse(w, http.StatusBadRequest, fmt.Sprintf("ExecuteQueryCreateItem:: %v", err.Error()))
+		return
+	}
+	insertedId, err := resp.LastInsertId()
+	if err != nil {
+		APIErrorResponse(w, http.StatusBadRequest, fmt.Sprintf("FailedToGetLastInsertedId:: %v", err.Error()))
+		return
+	}
+	newItem.ID = insertedId
 	w.Header().Set("Content-Type", "application/json")
-	items = append(items, newItem)
-	json.NewEncoder(w).Encode(items)
+	json.NewEncoder(w).Encode(newItem)
 }
 
 // update items handler
 func UpdateItem(w http.ResponseWriter, r *http.Request) {
 	var updateItem Item
-	if len(items) == 0 {
-		APIErrorResponse(w, http.StatusNotFound, "no items found")
-		return
-	}
 	err := json.NewDecoder(r.Body).Decode(&updateItem)
 	if err != nil {
 		APIErrorResponse(w, http.StatusBadRequest, err.Error())
 		return
 	}
 
-	for i, item := range items {
-		if item.ID == updateItem.ID {
-			items[i] = updateItem
-			w.Header().Set("Content-Type", "application/json")
-			json.NewEncoder(w).Encode(items)
-			return
-		}
+	stmt, err := db.Prepare(`UPDATE items SET name=(?) WHERE id=(?)`)
+	if err != nil {
+		APIErrorResponse(w, http.StatusInternalServerError, fmt.Sprintf("FailedToUpdateQuery:: %v", err.Error()))
+		return
 	}
-	APIErrorResponse(w, http.StatusBadRequest, "something went wrong")
+	defer stmt.Close()
+	_, err = stmt.Exec(updateItem.Name, updateItem.ID)
+	if err != nil {
+		APIErrorResponse(w, http.StatusInternalServerError, fmt.Sprintf("FailedToExecuteUpdateQuery:: %v", err.Error()))
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(updateItem)
 }
 
 // delete items handler
 func DeleteItem(w http.ResponseWriter, r *http.Request) {
 	var deleteItem Item
-	if len(items) == 0 {
-		APIErrorResponse(w, http.StatusNotFound, "items not found")
-		return
-	}
-
 	err := json.NewDecoder(r.Body).Decode(&deleteItem)
 	if err != nil {
 		APIErrorResponse(w, http.StatusBadRequest, err.Error())
 		return
 	}
-
-	for i, item := range items {
-		if item.ID == deleteItem.ID {
-			items = append(items[:i], items[i+1:]...)
-			w.Header().Set("Content-Type", "application/json")
-			json.NewEncoder(w).Encode(items)
-			return
-		}
+	stmt, err := db.Prepare(`DELETE FROM items WHERE id=(?)`)
+	if err != nil {
+		APIErrorResponse(w, http.StatusInternalServerError, fmt.Sprintf("FailedToDeleteQuery:: %v", err.Error()))
+		return
 	}
-	APIErrorResponse(w, http.StatusBadRequest, "something went wrong")
+	defer stmt.Close()
+	_, err = stmt.Exec(deleteItem.ID)
+	if err != nil {
+		APIErrorResponse(w, http.StatusInternalServerError, fmt.Sprintf("FailedToExecuteDeleteQuery:: %v", err.Error()))
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	respData := map[string]interface{}{
+		"message":    "Successfully Deleted",
+		"deleted_id": deleteItem.ID,
+	}
+	json.NewEncoder(w).Encode(respData)
 }
 
 // search items handler
 func SearchItem(w http.ResponseWriter, r *http.Request) {
-	var searchItem SearchItemType
-	if len(items) == 0 {
-		APIErrorResponse(w, http.StatusBadRequest, "items not found")
-		return
-	}
+	var searchItem Item
 	err := json.NewDecoder(r.Body).Decode(&searchItem)
 	if err != nil {
 		APIErrorResponse(w, http.StatusBadRequest, err.Error())
 		return
 	}
-
-	for _, item := range items {
-		if item.Name == searchItem.Name {
-			w.Header().Set("Content-Type", "application/json")
-			json.NewEncoder(w).Encode(item)
+	row, err := db.Query(`SELECT * FROM items WHERE name LIKE ?`, "%"+searchItem.Name+"%")
+	if err != nil {
+		APIErrorResponse(w, http.StatusInternalServerError, fmt.Sprintf("FailedToSearchQuery:: %v", err.Error()))
+		return
+	}
+	defer row.Close()
+	var response []Item
+	for row.Next() {
+		var item Item
+		err := row.Scan(&item.ID, &item.Name)
+		if err != nil {
+			APIErrorResponse(w, http.StatusInternalServerError, fmt.Sprintf("Database Error-searchscanitem:: %v", err.Error()))
 			return
 		}
+		response = append(response, item)
 	}
-
-	APIErrorResponse(w, http.StatusBadRequest, fmt.Sprintf("SearchItem Not found:: %v", searchItem.Name))
+	if response == nil {
+		APIErrorResponse(w, http.StatusNotFound, "items not found")
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
 }
 
 const PORT = ":8080"
