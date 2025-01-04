aa0f0dff7083e130a446fd672baa4281ed78a55a
Akash Kumar <akp31378@gmail.com>
2025-01-04 21:45:08 +0530

    golang restapi autehnication system using jwt

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
-module main.go
+module golang-restapi
 
 go 1.23.4
+
+require github.com/mattn/go-sqlite3 v1.14.24
+
+require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,43 +1,100 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
+	"time"
 
-	"main.go/helpers"
-	"main.go/json"
+	"github.com/golang-jwt/jwt/v5"
 )
 
-const numPool = 100
+// Secret key for signin using JWT token
+var jwtSecret = []byte("my-secret-key")
 
-func CalculateNumber(intChan chan int) {
-	randomNumber := helpers.RandomNumber(numPool)
-	intChan <- randomNumber
+// user struct for authentication
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
 }
 
+// Generate JWT Token
+func generateToken(username string) (string, error) {
+	tokenClaim := jwt.MapClaims{
+		"username": username,
+		"exp":      time.Now().Add(time.Hour * 2).Unix(),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, tokenClaim)
+	return token.SignedString(jwtSecret)
+}
+
+// Middleware to verify JWT token
+func authenticate(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		prefix := "Bearer "
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
+			http.Error(w, "Authorization header is Required", http.StatusUnauthorized)
+			return
+		}
+		if len(authHeader) <= len(prefix) {
+			http.Error(w, "invalid authorization header: token missing", http.StatusBadRequest)
+			return
+		}
+		tokenString := authHeader[len(prefix):]
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("unexpected signin method")
+			}
+			return jwtSecret, nil
+		})
+		if err != nil || !token.Valid {
+			http.Error(w, "Invalid token", http.StatusUnauthorized)
+			return
+		}
+		next.ServeHTTP(w, r)
+	}
+}
+
+// Login Handler
+func login(w http.ResponseWriter, r *http.Request) {
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, "Invalid JSON format", http.StatusBadRequest)
+		return
+	}
+
+	// Dummy username and password
+	if user.Username == "username" && user.Password == "password" {
+		token, err := generateToken(user.Username)
+		if err != nil {
+			http.Error(w, "Failed to generate token", http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]string{"token": token})
+		return
+	}
+	http.Error(w, "Invalid credentials", http.StatusBadRequest)
+}
+
+// Protect endpoint
+func protectedEndpoint(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{"message": "welcome to protected endpoint"})
+}
+
+const PORT = ":8080"
+
 func main() {
-	// First()
-	// VariableZeroValueBlankIdentifier()
-	// TypeConversionScopeHousekeeping()
-	// PrintfForDecimalHexadecimal()
-	// Pointers()
-	// TypeStruct()
-	// ReceiverStructGo()
-	// MapSlices()
-	// DecisionMaking()
-	// Loops()
-	// Interface()
-	// var myVar helpers.SomeType
-	// myVar.TypeName = "Package"
-	// myVar.TypeNumber = 2
-	// fmt.Println(myVar)
-
-	// Channel
-	intChan := make(chan int)
-	defer close(intChan)
-	go CalculateNumber(intChan)
-	num := <-intChan
-	fmt.Println(num)
-
-	// Json-formatter
-	json.JSONFormat()
+	// routes
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/protected-endpoint", authenticate(protectedEndpoint))
+	// starting server
+	fmt.Printf("Server is starting at http::/127.0.0.1:%s\n", PORT)
+	err := http.ListenAndServe(PORT, nil)
+	if err != nil {
+		fmt.Println("Error Occur while server", err)
+	}
 }
