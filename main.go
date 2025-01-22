8a2cd7a82c6c05d3e654639e37ff1b8e8b301636
Akash Kumar <akp31378@gmail.com>
2025-01-22 23:56:24 +0530

    if-else

--- main.go
+++ main.go
@@ -3,5 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	age := 14
+	if age >= 18 {
+		fmt.Println("Adult")
+	} else {
+		fmt.Println("Not an Adult")
+	}
+
+	budget := 2000
+	if budget >= 5000 {
+		fmt.Println("Go to Trip")
+	} else if budget >= 1000 && budget < 5000 {
+		fmt.Println("Go for Shopping")
+	} else {
+		fmt.Println("Stay home")
+	}
+
+	// declare variable in if construct
+	if age := 12; age >= 18 {
+		fmt.Println("Adult")
+	} else if age >= 12 {
+		fmt.Println("Teenager")
+	} else {
+		fmt.Println("not an adult")
+	}
+
 }
