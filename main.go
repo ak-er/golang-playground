98f3cc77fc1e2cb4adc007a0382a75a966d909a2
Akash Kumar <akp31378@gmail.com>
2025-01-22 23:37:58 +0530

    constant

--- main.go
+++ main.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	const greet = "Hello Golang!"
+	// Invalid to reasign  the constant variable
+	// greet = "Hello Python"  // get an error => cannot assign to greet (neither addressable nor a map index expression)
+	fmt.Println(greet)
+
+	// constant grouping
+	const (
+		port = 8000
+		host = "127.0.0.1"
+	)
+	fmt.Println(port, host)
 }
