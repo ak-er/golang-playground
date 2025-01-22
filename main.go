51f29ceeafba814e8821dd21c2cbf6c9a4c548c0
Akash Kumar <akp31378@gmail.com>
2025-01-22 23:46:53 +0530

    for-loop

--- main.go
+++ main.go
@@ -2,6 +2,31 @@ package main
 
 import "fmt"
 
+// for -> only construct of looping
 func main() {
-	fmt.Println("Hello World")
+	// use while loop using for loop
+	i := 1
+	for i <= 5 {
+		fmt.Println(i)
+		i++
+	}
+
+	// for loop complete syntax
+	/*
+		for initilization; condition; increament/decreament {
+			// excutable code
+		}
+	*/
+
+	for j := 1; j <= 5; j++ {
+		if j == 3 {
+			continue
+		}
+		fmt.Println(j)
+	}
+	fmt.Println("================")
+	// golang 1.22 version = range function
+	for k := range 3 {
+		fmt.Println(k)
+	}
 }
