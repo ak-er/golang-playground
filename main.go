9c27b511af993ba8c79e0fbaed62ae6cfea1324b
Akash Kumar <akp31378@gmail.com>
2025-01-23 22:31:25 +0530

    switch statement

--- main.go
+++ main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	// simple switch  statement
+	i := 3
+	switch i {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	default:
+		fmt.Println("other")
+	}
+
+	// multiple condition switch statement
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("weekend")
+	default:
+		fmt.Println("work days")
+	}
+
+	// type switch
+	whoAmI := func(i interface{}) {
+		switch i.(type) {
+		case int:
+			fmt.Println("Integer")
+		case string:
+			fmt.Println("String")
+		case bool:
+			fmt.Println("Boolean")
+		default:
+			fmt.Println("Other")
+		}
+	}
+
+	whoAmI(34)
+	whoAmI("golang")
+	whoAmI(false)
+	whoAmI(34.23)
+
 }
