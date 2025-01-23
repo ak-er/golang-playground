3ad7bf48f83ed25421a4677659091b8f74c99897
Akash Kumar <akp31378@gmail.com>
2025-01-23 22:53:44 +0530

    arrays

--- main.go
+++ main.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	// create an array
+	var arr [4]int
+	// assign value
+	arr[2] = 34
+	fmt.Println(arr)
+
+	// length of array
+	fmt.Println(len(arr))
+
+	// another method to create an array
+	arrr := [4]int{1, 2, 3, 4}
+	fmt.Println(arrr)
+
+	// 2d array
+	ararr := [2][2]int{{1, 2}, {3, 4}}
+	fmt.Println(ararr)
 }
