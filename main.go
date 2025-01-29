c65e1df8d35eba1e0492adb518d663757dadc47a
Akash Kumar <akp31378@gmail.com>
2025-01-29 23:44:38 +0530

    struct embedding

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+type Customer struct {
+	name  string
+	phone string
+}
+
+func newCustomer(name string, phone string) *Customer {
+	customer := Customer{
+		name:  name,
+		phone: phone,
+	}
+	return &customer
+}
+
+type Order struct {
+	id       string
+	amount   float32
+	status   string
+	createAt time.Time // nanasecond precision
+	customer Customer
+}
 
 func main() {
-	fmt.Println("Hello World")
+	order := Order{
+		id:       "1",
+		amount:   23.232,
+		status:   "pending",
+		customer: *newCustomer("john", "1234567890"),
+	}
+	order.createAt = time.Now()
+	fmt.Println(order)
+	fmt.Println(order.customer.name)
+
 }
