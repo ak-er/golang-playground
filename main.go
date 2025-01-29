0ff52f9750638dd5cc245b814e13780c3ffa2f87
Akash Kumar <akp31378@gmail.com>
2025-01-29 21:31:40 +0530

    range

--- main.go
+++ main.go
@@ -2,6 +2,34 @@ package main
 
 import "fmt"
 
+// iterating over data structure
 func main() {
-	fmt.Println("Hello World")
+	slice := []int{1, 2, 3, 4, 5, 6}
+	// fmt.Println(slice)
+	// iterate using for loop
+	/*
+		for i := 0; i < len(slice); i++ {
+			fmt.Println(slice[i])
+		}
+	*/
+
+	// iterate slice using range
+	sum := 0
+	for _, val := range slice {
+		// fmt.Println(val)
+		sum += val
+	}
+	fmt.Println(sum)
+
+	// iterate map using range
+	m := map[string]string{"fname": "Akash", "lname": "kumar"}
+	for k, v := range m {
+		fmt.Println(k, v)
+	}
+
+	// iterate string using range - unicode value for character
+	str := "😊learning golang"
+	for i, c := range str {
+		fmt.Println(i, string(c))
+	}
 }
