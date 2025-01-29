6296056792b54f3e8f0c48bae029fef6fd0ab01e
Akash Kumar <akp31378@gmail.com>
2025-01-29 23:00:03 +0530

    pointers

--- main.go
+++ main.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+func changeValue(num *int) {
+	*num = 5
+	fmt.Println("num value in changeValue func:", *num)
+}
+
 func main() {
-	fmt.Println("Hello World")
+	num := 3
+	changeValue(&num)
+	fmt.Println("num value after changeValue in main-func", num)
 }
