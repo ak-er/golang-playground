bbc2e79129ab3896457026c87f09faced7650da9
Akash Kumar <akp31378@gmail.com>
2025-01-29 21:47:48 +0530

    functions

--- main.go
+++ main.go
@@ -2,6 +2,37 @@ package main
 
 import "fmt"
 
+func addition(a, b int) int {
+	return a + b
+}
+
+/*
+func addingTwo(fn func(a int) int) int {
+	return fn(4)
+}
+*/
+
+func addingTwo() func(a int) int {
+	return func(a int) int {
+		return a + 2
+	}
+}
+
 func main() {
-	fmt.Println("Hello World")
+	a := 10
+	b := 34
+	result := addition(a, b)
+	fmt.Println(result)
+
+	/*
+		fn := func(a int) int {
+			return a + 2
+		}
+
+		x := addingTwo(fn)
+		fmt.Println(x)
+	*/
+
+	x := addingTwo()(5)
+	fmt.Println(x)
 }
