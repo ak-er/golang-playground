cf76a6e0c71dcfc40a1104f09799e5587e119b47
Akash Kumar <akp31378@gmail.com>
2025-01-29 22:41:53 +0530

    variadic functions

--- main.go
+++ main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+func sum(nums ...int) int {
+	sum := 0
+	for _, v := range nums {
+		sum += v
+	}
+	return sum
+}
+
 func main() {
-	fmt.Println("Hello World")
+	result := sum(3, 2, 1, 4, 5, 6, 4)
+	fmt.Println(result)
+
+	// using slice
+	s := []int{1, 2, 3, 4, 5}
+	result = sum(s...)
+	fmt.Println(result)
 }
