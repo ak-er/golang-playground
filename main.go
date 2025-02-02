258542e0758378878e32a1527d58b793e5f95293
Akash Kumar <akp31378@gmail.com>
2025-02-02 12:00:57 +0530

    enums

--- main.go
+++ main.go
@@ -2,6 +2,30 @@ package main
 
 import "fmt"
 
+// enumerated types
+
+/*
+type PaymentStatus int
+
+const (
+	Failed PaymentStatus = iota
+	Pending
+	Success
+)
+*/
+
+type PaymentStatus string
+
+const (
+	Failed  PaymentStatus = "failed"
+	Pending               = "pending"
+	Success               = "success"
+)
+
+func checkPaymentStatus(status PaymentStatus) {
+	fmt.Println("payment status:", status)
+}
+
 func main() {
-	fmt.Println("Hello World")
+	checkPaymentStatus(Pending)
 }
