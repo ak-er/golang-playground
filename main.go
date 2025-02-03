482e65eccd83a7e3d8ad60a6c090c95e19ca8af3
Akash Kumar <akp31378@gmail.com>
2025-02-04 00:13:01 +0530

    mutex

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
+
+type Post struct {
+	views int
+	mu    sync.Mutex
+}
+
+func (p *Post) inc(wg *sync.WaitGroup) {
+	defer func() {
+		wg.Done()
+		p.mu.Unlock()
+	}()
+	p.mu.Lock()
+	p.views += 1
+
+}
 
 func main() {
-	fmt.Println("Hello World")
+	var wg sync.WaitGroup
+	post := Post{views: 0}
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go post.inc(&wg)
+	}
+	wg.Wait()
+	fmt.Println(post.views)
 }
